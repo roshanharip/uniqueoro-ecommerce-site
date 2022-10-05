abe5f265ee78820d0978a3f403682f6718f7ce80
= <=>
2022-10-05 20:10:15 +0530

    Updating the Categories Component to it

---
+++ src/components/categories/categories-component.jsx
@@ -0,0 +1,24 @@
+import './categories-component.scss';
+const Categories = ({categories})=>{
+
+   return(      
+      <div className="categories">
+         <div className="categories-container">
+            { categories.map((category,index)=>
+               (
+                  <div key={index} className={`category-container category${category.id}`}>
+                     <div className="backgroundImage" style={
+                        {backgroundImage: `url(${category.imageUrl})`}
+                     } />
+                     <div className={`category-body-container ${category.title}`}>
+                        <h2>{category.title}</h2>
+                        <p>Book Now</p>
+                     </div>
+                  </div>
+               ))   
+            }   
+         </div>
+      </div>
+   )
+}
+export default Categories;
